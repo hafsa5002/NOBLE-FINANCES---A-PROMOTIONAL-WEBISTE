c5cbc3a2adce3c3216646f8524534a4b53f0f9ed
hafsa <nadeemhafsa704@gmail.com>
2025-08-12 21:36:00 +0500

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/**/*.{html,ejs,js}",
+     "./public/**/*.{js,css}",
+     "./src/**/*.{html,js,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
